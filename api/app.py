5296df4a3ab28e42a1f7dc0c4d543d2f0fc5b48a
Andrea Gonzalez <andy-rivaille@andyrivaille-ubuntu.tail19f965.ts.net>
2026-05-09 19:24:32 -0600

    Conectar api a bdd cloud

--- api/app.py
+++ api/app.py
@@ -1,45 +1,85 @@
 from flask import Flask, jsonify
+import os
+import psycopg2
 
 app = Flask(__name__)
 
-# Datos simulados AQI
-latest_reading = {
-    "temperature": 25.3,
-    "humidity": 48,
-    "pm25": 28,
-    "pm10": 35,
-    "co": 210,
-    "o3": 31,
-    "aqi_status": "Moderate"
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
 
 @app.route("/")
 def home():
     return jsonify({
-        "project": "AQI Monitoreo PoC",
+        "project": "AQI Cloud Monitoring PoC",
         "status": "running",
-        "message": "Middleware API funcionando correctamente"
+        "database": "connected"
     })
 
 @app.route("/api/latest")
 def latest():
-    return jsonify(latest_reading)
+
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute("""
+            SELECT temperature, humidity, pm25, pm10, co, o3, aqi_status
+            FROM readings
+            ORDER BY id DESC
+            LIMIT 1;
+        """)
+
+        row = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        if row:
+            return jsonify({
+                "temperature": row[0],
+                "humidity": row[1],
+                "pm25": row[2],
+                "pm10": row[3],
+                "co": row[4],
+                "o3": row[5],
+                "aqi_status": row[6]
+            })
+
+        return jsonify({"message": "No data found"})
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
 
 @app.route("/api/summary")
 def summary():
-    return jsonify({
-        "location": "DCEyC",
-        "devices": 1,
-        "active_sensors": [
-            "PM2.5",
-            "PM10",
-            "CO",
-            "O3",
-            "Temperature",
-            "Humidity"
-        ],
-        "aqi_status": latest_reading["aqi_status"]
-    })
+
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute("SELECT COUNT(*) FROM readings;")
+        total = cur.fetchone()[0]
+
+        cur.close()
+        conn.close()
+
+        return jsonify({
+            "project": "AQI Cloud Monitoring PoC",
+            "records": total,
+            "database": "PostgreSQL Cloud"
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
